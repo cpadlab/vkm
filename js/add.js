57478c9b0ef065bc574e64e72bcdde418e0eece4
Wual1014 <cpadlab@gmail.com>
2023-04-22 14:03:34 +0200

    Pre V3 Update 22.04.23 | 02

---
+++ js/add.js
@@ -0,0 +1,9 @@
+const addBtn = document.getElementById('add-btn');
+const overlay = document.getElementById('overlay');
+const inputss = document.getElementById('dv-nw-psw-site');
+ 
+addBtn.addEventListener('click', function() {
+  event.preventDefault();
+  inputss.style.display = 'block';
+  overlay.style.display = 'block';
+});
